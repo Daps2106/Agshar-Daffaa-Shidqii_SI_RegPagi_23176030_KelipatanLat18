2777f4624c9831282360d8d783837a3b07ffd5c9
Daps2106 <khabibsyekh22@gmail.com>
2024-11-04 11:06:03 +0700

    Add files via upload

---
+++ KelipatanLat18.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.percobaan1;
+
+/*
+ * Nama     : Agshar Daffaa Shidqii
+ * NIM      : 23176030
+ * Prodi    : Sistem Informasi
+ */
+public class KelipatanLat18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      for(double kelipatan = 3.5; kelipatan <= 35; kelipatan+= 3.5){
+        System.out.println(kelipatan);
+    }
+    }
+}
